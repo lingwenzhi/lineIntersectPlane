9803c88352e3cecf69a5190b6497b3a458f7ab9e
pengxiangwen <pengxiangwen@b.360.cn>
2018-05-15 18:38:43 +0800

    添加注释

--- lineIntersectPlane.js
+++ lineIntersectPlane.js
@@ -22,15 +22,24 @@ function generateCode(x, y, xmin, xmax, ymin, ymax) {
   return code
 }
 
+/* 判读线段相交，主要通过判断线段端点是否在线段异侧，已一条线段为起点，沿着线段方向，在左手边为逆时针方向，右手边为顺时针方向
+* 如果线段A两个端点分别在B线段的两侧，同时B线段的两个端点也在线段A的两侧，则线段A和B相交。
+* 主要通过向量叉积进行判读，叉积大于0，改点在向量顺时针方向，小于0，在逆时针方向，等于0，在直线上
+* 线段A: a(x, y)、b(x, y) 线段B：c(x, y)、d(x, y)
+* u => (c - a) x (b - a)
+* v => (d - a) x (b - a)
+* w => (a - c) x (d - c)
+* z => (b - c) x (d - c)
+*/
 function crossLine(a, b, c, d) {
   // 判读线段（a, b）和线段（c, d）是否相交
   const u = (c.x - a.x) * (b.y - a.y) - (b.x - a.x) * (c.y - a.y)
   const v = (d.x - a.x) * (b.y - a.y) - (b.x - a.x) * (d.y - a.y)
   const w = (a.x - c.x) * (d.y - c.y) - (d.x - c.x) * (a.y - c.y)
   const z = (b.x - c.x) * (d.y - c.y) - (d.x - c.x) * (b.y - c.y)
-  return u * v <= 0.00000001 && w * z <= 0.00000001
+  return u * v < 0 && w * z < 0
 }
-export function lineIntersectPlane (line = { source: { x: 0, y: 0 }, target: { x: 0, y: 0 } }, plane = [0, 0, 1, 1]) {
+export function lineIntersectPlane (line = { source: { x: 0, y: 0 }, target: { x: 0, y: 0 } }, plane = [0, 1, 0, 1]) {
   const xMin = plane[0]
   const xMax = plane[1]
   const yMin = plane[2]
